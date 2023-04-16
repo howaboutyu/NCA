6c060a0f39713014cc7de73487fde5a0e604f5e6
higgsboost <higgsboost@gmail.com>
2023-04-16 22:24:32 +0800

    Added `create_gif` and `create_video` functions

--- nca/utils.py
+++ nca/utils.py
@@ -1,7 +1,11 @@
 import jax.numpy as jnp
+import numpy as np
+from moviepy.editor import ImageSequenceClip
+import tempfile
+from glob import glob
+import cv2
 
-
-import jax.numpy as jnp
+from typing import List, Any, Union
 
 
 def NCWH_to_NHWC(x: jnp.ndarray) -> jnp.ndarray:
@@ -40,3 +44,51 @@ def alpha_mask(x: jnp.ndarray) -> jnp.ndarray:
     # clip to [0, 1]
     x = jnp.clip(x, 0, 1)
     return x[:, :, :, :3] * x[:, :, :, 3:4]
+
+
+def make_gif(
+    images: Union[List[Any], np.ndarray], filename: str, fps: int = 10
+) -> None:
+    """Creates a movie from a list of images.
+
+    Args:
+        images: A list of images.
+        filename: The name of the GIF file.
+        fps: The number of frames per second. Default is 10.
+
+    """
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # write images to tempdir
+        for i, image in enumerate(images):
+            image = np.asarray(image).astype(np.uint8)
+
+            cv2.imwrite(f"{tempdir}/{i}.png", image)
+
+        # create gif
+        clip = ImageSequenceClip(glob(f"{tempdir}/*.png"), fps=fps)
+        clip.write_gif(filename, fps=fps)
+
+
+def make_video(
+    images: Union[List[Any], np.ndarray], filename: str, fps: int = 10
+) -> None:
+    """Creates a movie from a list of images.
+
+    Args:
+        images: A list of images.
+        filename: The name of the GIF file.
+        fps: The number of frames per second. Default is 10.
+
+    """
+
+    with tempfile.TemporaryDirectory() as tempdir:
+        # write images to tempdir
+        for i, image in enumerate(images):
+            image = np.asarray(image).astype(np.uint8)
+
+            cv2.imwrite(f"{tempdir}/{i}.png", image)
+
+        # create gif
+        clip = ImageSequenceClip(glob(f"{tempdir}/*.png"), fps=fps)
+        clip.write_videofile(filename, fps=fps)
---
+++ tests/test_utils.py
@@ -0,0 +1,33 @@
+from context import *
+
+import pytest
+import jax
+import numpy as np
+import os
+from nca.utils import make_gif, make_video
+
+
+def test_make_gif():
+    key = jax.random.PRNGKey(0)
+    x = jax.random.normal(key, (10, 32, 32, 3))
+    x = (x + 1.0) / 2.0
+    x = (x * 255).astype(np.uint8)
+
+    x_array = [x_ for x_ in x]
+
+    make_gif(x_array, "/tmp/test.gif")
+
+    assert os.path.exists("/tmp/test.gif")
+
+
+def test_make_video():
+    key = jax.random.PRNGKey(0)
+    x = jax.random.normal(key, (10, 320, 320, 3))
+    x = (x + 1.0) / 2.0
+    x = (x * 255).astype(np.uint8)
+
+    x_array = [x_ for x_ in x]
+
+    make_video(x_array, "/tmp/test.mp4")
+
+    assert os.path.exists("/tmp/test.mp4")
