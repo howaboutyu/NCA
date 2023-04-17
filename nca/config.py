56d017e43a31e07cbc5ed3f9a787af39aaeabca7
higgsboost <higgsboost@gmail.com>
2023-04-17 22:12:42 +0800

    Using `steps` instead of `epochs`

--- nca/config.py
+++ nca/config.py
@@ -6,6 +6,7 @@ class NCAConfig:
     dimensions: tuple = (40, 40)
     model_output_len: int = 16
     batch_size: int = 16
-    num_epochs: int = 100000
-    steps_per_epoch: int = 100
+    num_steps: int = 100000
     learning_rate: float = 1e-4
+    pool_size: int = 100
+    target_filename: str = "emoji_imgs/skier.png"
