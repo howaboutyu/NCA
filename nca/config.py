491fa1a09672247fd9c0551f32d389ed2d16fd0d
howaboutyu <>
2023-04-18 15:57:19 +0000

    Increase frequency for ckpt saving

--- nca/config.py
+++ nca/config.py
@@ -6,14 +6,14 @@ import yaml
 class NCAConfig:
     dimensions: tuple = (40, 40)
     model_output_len: int = 16
-    batch_size: int = 16
+    batch_size: int = 8
     num_steps: int = 100000
-    eval_every: int = 1000
-    learning_rate: float = 1e-4
+    eval_every: int = 30
+    learning_rate: float = 1e-5
     pool_size: int = 100
     target_filename: str = "emoji_imgs/skier.png"
     checkpoint_dir: str = "checkpoints"
-    checkpoint_every: int = 1000
+    checkpoint_every: int = 30
     validation_video_dir: str = "validation_videos"
     log_dir: str = "logs"
     log_every: int = 1
