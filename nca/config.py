38193fd9b99af17c08cdc778bc66f1fa887411ef
higgsboost <higgsboost@gmail.com>
2023-04-18 21:32:09 +0800

    Added `eval_every`, `checkpoint_*`, and other config params

--- nca/config.py
+++ nca/config.py
@@ -7,6 +7,11 @@ class NCAConfig:
     model_output_len: int = 16
     batch_size: int = 16
     num_steps: int = 100000
+    eval_every: int = 1000
     learning_rate: float = 1e-4
     pool_size: int = 100
     target_filename: str = "emoji_imgs/skier.png"
+    checkpoint_dir: str = "checkpoints"
+    checkpoint_every: int = 1000
+    validation_video_dir: str = "validation_videos"
+    tensorboard_dir: str = "logs"
