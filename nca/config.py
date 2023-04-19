582b094708c6cecb2ab0451f03f39bf5a9e9b2a4
howaboutyu <>
2023-04-19 13:34:07 +0000

    New emoji!!!! 😝😝😝😝😝😝😝😝😝

--- nca/config.py
+++ nca/config.py
@@ -6,17 +6,17 @@ import yaml  # type: ignore
 class NCAConfig:
     dimensions: tuple = (40, 40)
     model_output_len: int = 16
-    batch_size: int = 8
+    batch_size: int = 16
     num_steps: int = 100000
     eval_every: int = 30
-    learning_rate: float = 1e-5
-    pool_size: int = 100
-    target_filename: str = "emoji_imgs/skier.png"
+    learning_rate: float = 1e-3
+    pool_size: int = 1000
+    target_filename: str = "emoji_imgs/smile.png"
     checkpoint_dir: str = "checkpoints"
     checkpoint_every: int = 30
     validation_video_dir: str = "validation_videos"
     log_dir: str = "logs"
-    log_every: int = 1
+    log_every: int = 10
 
 
 def load_config(config_file: str) -> NCAConfig:
