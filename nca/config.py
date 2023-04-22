03900a64caac4e90d9d9d77040def5f8fdffa4d3
howaboutyu <>
2023-04-22 13:05:26 +0000

    Increased logging duration

--- nca/config.py
+++ nca/config.py
@@ -4,19 +4,19 @@ import yaml  # type: ignore
 
 @dataclass
 class NCAConfig:
-    dimensions: tuple = (40, 40)
+    dimensions: tuple = (56, 56)
     model_output_len: int = 16
     batch_size: int = 16
     num_steps: int = 100000
-    eval_every: int = 50
-    learning_rate: float = 1e-5
+    eval_every: int = 500
+    learning_rate: float = 2e-4
     pool_size: int = 1000
-    target_filename: str = "emoji_imgs/smile.png"
+    target_filename: str = "emoji_imgs/skier.png"
     checkpoint_dir: str = "checkpoints"
-    checkpoint_every: int = 50
+    checkpoint_every: int = 500
     validation_video_dir: str = "validation_videos"
     log_dir: str = "logs"
-    log_every: int = 50
+    log_every: int = 500
 
 
 def load_config(config_file: str) -> NCAConfig:
