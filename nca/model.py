b9f4cd26deff251876166a7ec838a4e273326185
howaboutyu <>
2023-04-27 16:42:28 +0000

    Added `num_layers` for  dense->relu block replication

--- nca/model.py
+++ nca/model.py
@@ -1,14 +1,19 @@
 import jax.numpy as jnp
 import flax.linen as nn
+from typing import List
 
 
 class UpdateModel(nn.Module):
     model_output_len: int = 16
+    num_layers: int = 3  # number of dense->relu layers
 
     def setup(self):
         """Initialize the model layers."""
-        self.dense1 = nn.Dense(128, kernel_init=nn.initializers.lecun_normal())
-        self.dense2 = nn.Dense(128, kernel_init=nn.initializers.lecun_normal())
+
+        self.dense_array = [
+            nn.Dense(64, kernel_init=nn.initializers.lecun_normal())
+            for _ in range(self.num_layers)
+        ]
 
         self.dense_final = nn.Dense(
             self.model_output_len, kernel_init=nn.initializers.zeros
@@ -23,9 +28,10 @@ class UpdateModel(nn.Module):
         Returns:
             A 2D tensor representing the output data with shape (batch_size, output_len).
         """
-        x = self.dense1(perception_vector)
-        x = nn.relu(x)
-        x = self.dense2(x)
-        x = nn.relu(x)
+        x = perception_vector
+
+        for dense in self.dense_array:
+            x = dense(x)
+            x = nn.relu(x)
         ds = self.dense_final(x)
         return ds
