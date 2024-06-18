5c3adc0b6db412f36f6f4513332ff726d565c42d
howaboutyu <>
2024-06-18 12:15:32 +0000

    New install script from latest docs

--- scripts/install_jax_gpu.sh
+++ scripts/install_jax_gpu.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-set -e 
-# Setup Jax on GPU
-pip install --upgrade "jax[cuda11_cudnn82]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+echo "Select the installation option for JAX:"
+echo "1. CPU"
+echo "2. GPU (NVIDIA, CUDA 12)"
+echo "3. TPU (Google Cloud TPU VM)"
+read -p "Enter your choice (1, 2, or 3): " choice
 
-echo You have successfully install JAX 😁 on GPU
+case $choice in
+    1)
+        echo "Installing JAX for CPU..."
+        pip install -U "jax[cpu]"
+        ;;
+    2)
+        echo "Installing JAX for GPU with CUDA 12..."
+        pip install -U "jax[cuda12]"
+        ;;
+    3)
+        echo "Installing JAX for TPU..."
+        pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+        ;;
+    *)
+        echo "Invalid selection. Please run the script again and select 1, 2, or 3."
+        ;;
+esac
+
